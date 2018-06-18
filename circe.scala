14e0b808dbe05e99b4c6c460c00c4feab23c08b7
wcrbrm <viclopata@gmail.com>
2018-06-18 10:02:49 +0300

    circe

---
+++ circe.scala
@@ -0,0 +1,6 @@
+val circe = Seq(
+  "io.circe" %%	"circe-core" % "0.9.2",
+  "io.circe" %% "circe-parser" % "0.9.2",
+  "io.circe" %% "circe-optics" % "0.9.2",
+  "de.heikoseeberger" %% "akka-http-circe" % "1.20.0"
+)
