63aed811e2a986759469d36bb1cb6f480b0f7275
wcrbrm <viclopata@gmail.com>
2018-06-18 10:23:18 +0300

    Apache Spark

---
+++ spark.scala
@@ -0,0 +1,5 @@
+val spark = Seq(
+  "org.apache.spark" %% "spark-core" % "1.2.0" % "provided",
+  "org.apache.spark" %% "spark-streaming" % "1.2.0" % "provided",
+  "org.apache.spark" % "spark-streaming-twitter_2.10" % "1.2.0"
+)
